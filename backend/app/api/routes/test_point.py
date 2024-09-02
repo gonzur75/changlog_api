e3ba7099f370ed055ca3239203341a57bdb87d88
Marcin Włodarczyk <gonzur75@gmail.com>
2024-09-02 11:06:59 +0200

    refactor test suit

---
+++ backend/app/api/routes/test_point.py
@@ -0,0 +1,77 @@
+from sqlalchemy.orm import Session
+from starlette.testclient import TestClient
+
+import app.enums
+from app import models
+from app.modules.config import settings
+from app.tests.utils import get_test_auth_header_and_user
+
+
+def test_delete_point(
+    client: TestClient,
+    user_factory,
+    product_factory,
+    update_factory,
+    update_point_factory,
+    session: Session,
+):
+    headers, user = get_test_auth_header_and_user(user_factory)
+    product = product_factory(owner=user)
+    update = update_factory(product=product)
+    update_point = update_point_factory(update=update)
+
+    response = client.delete(
+        f"{settings.API_version_string}points/{update_point.id}", headers=headers
+    )
+    assert response.status_code == 200
+    assert "Point deleted successfully" in response.text
+    check = (
+        session.query(models.UpdatePoint)
+        .filter(models.UpdatePoint.id == update.id)
+        .first()
+    )
+    assert check is None
+
+
+def test_patch_point(
+    client: TestClient,
+    user_factory,
+    product_factory,
+    update_factory,
+    update_point_factory,
+):
+    headers, user = get_test_auth_header_and_user(user_factory)
+    product = product_factory(owner=user)
+    update = update_factory(product=product)
+    update_point = update_point_factory(update=update)
+    json = {"name": "Dark Mode", "type": app.enums.UpdatePointType.FIXED.value}
+
+    response = client.patch(
+        f"{settings.API_version_string}points/{update_point.id}",
+        headers=headers,
+        json=json,
+    )
+    assert response.status_code == 200
+    print(response.request)
+    assert json["name"] in response.json()["name"]
+    assert update_point.type == app.enums.UpdatePointType.FIXED.value
+
+
+def test_retrieve_point(
+    client: TestClient,
+    user_factory,
+    product_factory,
+    update_factory,
+    update_point_factory,
+):
+    headers, user = get_test_auth_header_and_user(user_factory)
+    product = product_factory(owner=user)
+    update = update_factory(product=product)
+    update_point = update_point_factory(update=update)
+
+    response = client.get(
+        f"{settings.API_version_string}points/{update_point.id}", headers=headers
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert update_point.name == content["name"]
--- backend/app/tests/api/routes/test_product.py
+++ backend/app/tests/api/routes/test_product.py
@@ -1,5 +1,5 @@
 from app import enums, handlers, schemas
-from app.modules.config import API_version_string
+from app.modules.config import settings
 from app.tests.utils import get_test_auth_header_and_user
 from sqlalchemy.orm import Session
 from starlette.testclient import TestClient
@@ -13,7 +13,9 @@ def test_change_product_name(
     test_json = {"name": "new name"}
 
     response = client.patch(
-        f"{API_version_string}products/{product.id}", json=test_json, headers=header
+        f"{settings.API_version_string}products/{product.id}",
+        json=test_json,
+        headers=header,
     )
     assert response.status_code == 200
 
@@ -25,7 +27,7 @@ def test_retrieve_products(client: TestClient, user_factory, product_factory) ->
     headers, user = get_test_auth_header_and_user(user_factory)
 
     product_factory.create_batch(10, owner=user)
-    response = client.get(f"{API_version_string}products/", headers=headers)
+    response = client.get(f"{settings.API_version_string}products/", headers=headers)
 
     assert response.status_code == 200
     content = response.json()
@@ -36,7 +38,7 @@ def test_retrieve_product(client: TestClient, session: Session, user_factory) ->
     headers, user = get_test_auth_header_and_user(user_factory)
 
     response_item_not_found = client.get(
-        f"{API_version_string}products/1", headers=headers
+        f"{settings.API_version_string}products/1", headers=headers
     )
     assert response_item_not_found.status_code == 404
     assert enums.ExceptionMessages.not_found in response_item_not_found.text
@@ -50,13 +52,14 @@ def test_retrieve_product(client: TestClient, session: Session, user_factory) ->
     another_user_product = handlers.create_product(session, product2, another_user.id)
 
     response_another_user_item = client.get(
-        f"{API_version_string}products/{another_user_product.id}", headers=headers
+        f"{settings.API_version_string}products/{another_user_product.id}",
+        headers=headers,
     )
     assert response_another_user_item.status_code == 400
     assert "Not your product" in response_another_user_item.text
 
     response = client.get(
-        f"{API_version_string}products/{user_product.id}", headers=headers
+        f"{settings.API_version_string}products/{user_product.id}", headers=headers
     )
     assert response.status_code == 200
     content = response.json()
@@ -67,7 +70,9 @@ def test_create_product(client: TestClient, user_factory) -> None:
     jwt_token_header = get_test_auth_header_and_user(user_factory)[0]
     test_json = {"name": "composting.web"}
     response = client.post(
-        f"{API_version_string}products/", headers=jwt_token_header, json=test_json
+        f"{settings.API_version_string}products/",
+        headers=jwt_token_header,
+        json=test_json,
     )
 
     assert response.status_code == 201
@@ -75,3 +80,18 @@ def test_create_product(client: TestClient, user_factory) -> None:
     assert content["name"] == test_json["name"]
     assert "id" in content
     assert "owner_id" in content
+
+
+def test_get_product_updates(
+    client: TestClient, user_factory, product_factory, update_factory
+):
+    headers, user = get_test_auth_header_and_user(user_factory)
+    product = product_factory(owner=user)
+    update_factory.create_batch(10, product=product)
+    response = client.get(
+        f"{settings.API_version_string}products/{product.id}/updates/",
+        headers=headers,
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert len(content["data"]) == 10
